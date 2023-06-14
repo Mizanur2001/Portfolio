7433e21f306e3c3a2130f39c03fd2eb38caf84f4
Mizanur <jameslev2001@gmail.com>
2023-06-14 19:22:59 +0530

    connect contact form

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const dotenv = require('dotenv');
+dotenv.config()
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+const PORT = 2200
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   },
 });
 
@@ -24,23 +25,23 @@ contactEmail.verify((error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Ready to Send");
+    console.log("Connected Mail Server");
   }
 });
 
 router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+  const name = req.body.firstName + " " + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
   const mail = {
-    from: name,
-    to: "********@gmail.com",
+    from: process.env.EMAIL_USER,
+    to: process.env.SENT_MAIL,
     subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
+    html: `<p><strong>Name</strong>: ${name}</p>
+           <p><strong>Email</strong>: ${email}</p>
+           <p><strong>Phone</strong>: ${phone}</p>
+           <p><strong>Message</strong>: ${message}</p>`,
   };
   contactEmail.sendMail(mail, (error) => {
     if (error) {
