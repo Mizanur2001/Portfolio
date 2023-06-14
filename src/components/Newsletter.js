7257cf5dc18f02078fbb2d06f705f5ba284f4d51
Mizanur <jameslev2001@gmail.com>
2023-06-14 20:55:30 +0530

    create the subcribe section

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,46 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
 
-export const Newsletter = ({ status, message, onValidated }) => {
+export const Newsletter = () => {
   const [email, setEmail] = useState('');
-
-  useEffect(() => {
-    if (status === 'success') clearFields();
-  }, [status])
+  const [status, setStatus] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    email &&
-    email.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email
-    })
-  }
+    if(email===""){
+      return ;
+    }
+    setStatus(true)
 
-  const clearFields = () => {
-    setEmail('');
   }
 
   return (
-      <Col lg={12}>
-        <div className="newsletter-bx wow slideInUp">
-          <Row>
-            <Col lg={12} md={6} xl={5}>
-              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
-              {status === 'sending' && <Alert>Sending...</Alert>}
-              {status === 'error' && <Alert variant="danger">{message}</Alert>}
-              {status === 'success' && <Alert variant="success">{message}</Alert>}
-            </Col>
-            <Col md={6} xl={7}>
-              <form onSubmit={handleSubmit}>
-                <div className="new-email-bx">
-                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
-                  <button type="submit">Submit</button>
-                </div>
-              </form>
-            </Col>
-          </Row>
-        </div>
-      </Col>
+    <Col lg={12}>
+      <div className="newsletter-bx wow slideInUp">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
+          </Col>
+          <Col md={6} xl={7} >
+            <form onSubmit={handleSubmit}>
+              <div className="new-email-bx">
+                <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                <button type="submit">Submit</button>
+              </div>
+              {status && <Alert variant="warning" className="mt-6" style={{ borderRadius: '1rem' }}>Under Progress... :)</Alert>}
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Col>
   )
 }
