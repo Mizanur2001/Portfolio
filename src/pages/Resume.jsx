88911b9baf7650b298168ca6c53e03edddd1e241
Mizanur <jameslev2001@gmail.com>
2025-05-11 12:24:45 +0530

    feat: add react healmet

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ResumePdf from '../assets/CV/MizanurRahamanResume(V30.0.0).pdf'
+import { Helmet } from 'react-helmet'
 
 const Resume = () => {
     return (
         <div>
+            <Helmet>
+                <title>Resume | Mizanur Rahaman</title>
+                <meta name="description" content="View the resume of Mizanur Rahaman." />
+            </Helmet>
             <iframe
                 src={ResumePdf}
                 width="100%"
